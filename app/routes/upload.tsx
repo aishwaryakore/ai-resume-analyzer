d4051855e3135ba75a2b716e42268d73cfe4ee75
aishwaryakore <aishwaryakore07@gmail.com>
2026-01-13 06:07:44 -0500

    implemented feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -60,7 +60,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
-        navigate(`/resume/${uuid}`);
+        navigate(`/resume/${data.id}`);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
